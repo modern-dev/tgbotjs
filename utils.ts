1bfd1952dc9ce163a36dbd1c260740ad98a19b31
virtyaluk <bohdan@modern-dev.com>
2020-02-04 01:33:17 +0200

    feat: adds utils source.

---
+++ utils.ts
@@ -0,0 +1,19 @@
+/*!
+ * tgbotjs - Telegram Bot API for Node.js and browsers.
+ * https://github.com/modern-dev/tgbotjs
+ *
+ * Copyright (c) 2020 Bohdan Shtepan
+ * Licensed under the MIT license.
+ */
+
+const isObject = (obj: any): boolean => Object.prototype.toString.call(obj) === '[object Object]';
+const snakeCased = (str: string): string =>
+  str.replace(/([A-Z])/g, (g) => `_${g.toLocaleLowerCase()}`);
+const camelCased = (str: string): string =>
+  str.replace(/_([a-z])/g, (g) => g[1].toLocaleUpperCase());
+
+export {
+  isObject,
+  snakeCased,
+  camelCased
+};
