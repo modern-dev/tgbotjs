33eae28f21e9fea17c64d50bb6d9d0bf55f63cce
virtyaluk <bohdan@modern-dev.com>
2020-02-05 01:56:34 +0200

    feat: update basic types.

--- types.ts
+++ types.ts
@@ -1,16 +1,50 @@
+/*!
+ * tgbotjs - Telegram Bot API for Node.js and browsers.
+ * https://github.com/modern-dev/tgbotjs
+ *
+ * Copyright (c) 2020 Bohdan Shtepan
+ * Licensed under the MIT license.
+ */
+
+// eslint-disable-next-line no-unused-vars
+import { ReadStream } from 'fs';
+
+export type APIResponse<T> = {
+  ok: boolean;
+  result: T;
+  description: string;
+}
+
+export type ChatId = string | number;
+
+/* eslint-disable no-unused-vars */
+export enum ChatActions {
+  Typing = 'typing',
+  UploadPhoto = 'upload_photo',
+  RecordVideo = 'record_video',
+  UploadVideo = 'upload_video',
+  RecordAudio = 'record_audio',
+  UploadAudio = 'upload_audio',
+  UploadDocument = 'upload_document',
+  FindLocation = 'find_location',
+  RecordVideoNote = 'record_video_note',
+  UploadVideoNote = 'upload_video_note'
+}
+/* eslint-enable no-unused-vars */
+
 export type Update = {
-  update_id: number
+  updateId: number
   message: Message
-  edited_message: Message
-  channel_post: Message
-  edited_channel_post: Message
-  inline_query: InlineQuery
-  chosen_inline_result: ChosenInlineResult
-  callback_query: CallbackQuery
-  shipping_query: ShippingQuery
-  pre_checkout_query: PreCheckoutQuery
+  editedMessage: Message
+  channelPost: Message
+  editedChannelPost: Message
+  inlineQuery: InlineQuery
+  chosenInlineResult: ChosenInlineResult
+  callbackQuery: CallbackQuery
+  shippingQuery: ShippingQuery
+  preCheckoutQuery: PreCheckoutQuery
   poll: Poll
-  poll_answer: PollAnswer
+  pollAnswer: PollAnswer
 }
 
 export type InlineQuery = {
@@ -22,7 +56,7 @@ export type InlineQuery = {
 }
 
 export type ChosenInlineResult = {
-  result_id: string
+  resultId: string
   from: User
   location?: Location
   inlineMessageId?: string
@@ -32,44 +66,44 @@ export type ChosenInlineResult = {
 export type ShippingQuery = {
   id: string
   from: User
-  invoice_payload: string
-  shipping_address: ShippingAddress
+  invoicePayload: string
+  shippingAddress: ShippingAddress
 }
 
 export type ShippingAddress = {
-  country_code: string
+  countryCode: string
   state: string
   city: string
-  street_line1: string
-  street_line2: string
-  post_code: string
+  streetLine1: string
+  streetLine2: string
+  postCode: string
 }
 
 export type OrderInfo = {
   name: string
-  phone_number: string
+  phoneNumber: string
   email: string
-  shipping_address: ShippingAddress
+  shippingAddress: ShippingAddress
 }
 
 export type PreCheckoutQuery = {
   id: string
   from: User
   currency: string
-  total_amount: number
-  invoice_payload: string
-  shipping_option_id: string
-  order_info: OrderInfo
+  totalAmount: number
+  invoicePayload: string
+  shippingOptionId: string
+  orderInfo: OrderInfo
 }
 
 export type WebhookInfo = {
   url: string
-  has_custom_certificate: boolean
-  pending_update_count: number
-  last_error_date: number
-  last_error_message: string
-  max_connections: number
-  allowed_updates: Array<string>
+  hasCustomCertificate: boolean
+  pendingUpdateCount: number
+  lastErrorDate: number
+  lastErrorMessage: string
+  maxConnections: number
+  allowedUpdates: Array<string>
 }
 
 export type User = {
@@ -89,36 +123,36 @@ export type Chat = {
   type: string
   title: string
   username: string
-  first_name: string
-  last_name: string
+  firstName: string
+  lastName: string
   photo: ChatPhoto
   description: string
-  invite_link: string
-  pinned_message: Message
+  inviteLink: string
+  pinnedMessage: Message
   permissions: ChatPermissions
-  slow_mode_delay: number
-  sticker_set_name: string
-  can_set_sticker_set: boolean
+  slowModeDelay: number
+  stickerSetName: string
+  canSetStickerSet: boolean
 }
 
 export type Message = {
-  message_id: number
+  messageId: number
   from:	User
   date: number
   chat: Chat
-  forward_from:	User
-  forward_from_chat: Chat
-  forward_from_message_id: number
-  forward_signature: string
-  forward_sender_name: string
-  forward_date: number
-  reply_to_message:	Message
-  edit_date: number
-  media_group_id: string
-  author_signature: string
+  forwardFrom:	User
+  forwardFromChat: Chat
+  forwardFromMessageId: number
+  forwardSignature: string
+  forwardSenderName: string
+  forwardDate: number
+  replyToMessage:	Message
+  editDate: number
+  mediaGroupId: string
+  authorSignature: string
   text: string
   entities: Array<MessageEntity>
-  caption_entities:	Array<MessageEntity>
+  captionEntities:	Array<MessageEntity>
   audio: Audio
   document:	Document
   animation: Animation
@@ -127,28 +161,28 @@ export type Message = {
   sticker: Sticker
   video: Video
   voice: Voice
-  video_note: VideoNote
+  videoNote: VideoNote
   caption: string
   contact: Contact
   location: Location
   venue: Venue
   poll: Poll
-  new_chat_members: Array<User>
-  left_chat_member: User
-  new_chat_title: string
-  new_chat_photo: Array<PhotoSize>
-  delete_chat_photo: boolean
-  group_chat_created: boolean
-  supergroup_chat_created: boolean
-  channel_chat_created: boolean
-  migrate_to_chat_id: number
-  migrate_from_chat_id: number
-  pinned_message: Message
+  newChatMembers: Array<User>
+  leftChatMember: User
+  newChatTitle: string
+  newChatPhoto: Array<PhotoSize>
+  deleteChatPhoto: boolean
+  groupChatCreated: boolean
+  supergroupChatCreated: boolean
+  channelChatCreated: boolean
+  migrateToChatId: number
+  migrateFromChatId: number
+  pinnedMessage: Message
   invoice: Invoice
-  successful_payment: SuccessfulPayment
-  connected_website: string
-  passport_data: PassportData
-  reply_markup: InlineKeyboardMarkup
+  successfulPayment: SuccessfulPayment
+  connectedWebsite: string
+  passportData: PassportData
+  replyMarkup: InlineKeyboardMarkup
 }
 
 export type Game = {
@@ -156,45 +190,45 @@ export type Game = {
   description: string
   photo: Array<PhotoSize>
   text: string
-  text_entities: Array<MessageEntity>
+  textEntities: Array<MessageEntity>
   animation: Animation
 }
 
 export type Invoice = {
   title: string
   description: string
-  start_parameter: string
+  startParameter: string
   currency: string
-  total_amount: number
+  totalAmount: number
 }
 
 export type SuccessfulPayment = {
   currency: string
-  total_amount: number
-  invoice_payload: string
-  shipping_option_id: string
-  order_info: OrderInfo
-  telegram_payment_charge_id: string
-  provider_payment_charge_id: string
+  totalAmount: number
+  invoicePayload: string
+  shippingOptionId: string
+  orderInfo: OrderInfo
+  telegramPaymentChargeId: string
+  providerPaymentChargeId: string
 }
 
 export type Sticker = {
-  file_id: string
-  file_unique_id: string
+  fileId: string
+  fileUniqueId: string
   width: number
   height: number
-  is_animated: boolean
+  isAnimated: boolean
   thumb: PhotoSize
   emoji: string
-  set_name: string
-  mask_position: MaskPosition
-  file_size: number
+  setName: string
+  maskPosition: MaskPosition
+  fileSize: number
 }
 
 export type MaskPosition = {
   point: string
-  x_shift: number
-  y_shift: number
+  xShift: number
+  yShift: number
   scale: number
 }
 
@@ -206,21 +240,21 @@ export type PassportData = {
 export type EncryptedPassportElement = {
   type: string
   data: string
-  phone_number: string
+  phoneNumber: string
   email: string
   files: Array<PassportFile>
-  front_side: PassportFile
-  reverse_side: PassportFile
+  frontSide: PassportFile
+  reverseSide: PassportFile
   selfie: PassportFile
   translation: Array<PassportFile>
   hash: String
 }
 
 export type PassportFile = {
-  file_id: string
-  file_unique_id: string
-  file_size: number
-  file_date: number
+  fileId: string
+  fileUniqueId: string
+  fileSize: number
+  fileDate: number
 }
 
 export type EncryptedCredentials = {
@@ -239,80 +273,80 @@ export type MessageEntity = {
 }
 
 export type PhotoSize = {
-  file_id: string
-  file_unique_id: string
+  fileId: string
+  fileUniqueId: string
   width: number
   height: number
-  file_size: number
+  fileSize: number
 }
 
 export type Audio = {
-  file_id: string
-  file_unique_id: string
+  fileId: string
+  fileUniqueId: string
   duration: number
   performer: string
   title: string
-  mime_type: string
-  file_size: number
+  mimeType: string
+  fileSize: number
   thumb:	PhotoSize
 }
 
 // @ts-ignore
 export type Document = {
-  file_id: string
-  file_unique_id: string
+  fileId: string
+  fileUniqueId: string
   thumb:	PhotoSize
-  file_name: string
-  mime_type: string
-  file_size: number
+  fileName: string
+  mimeType: string
+  fileSize: number
 }
 
 export type Video = {
-  file_id: string
-  file_unique_id: string
+  fileId: string
+  fileUniqueId: string
   width: number
   height: number
   duration: number
   thumb:	PhotoSize
-  mime_type: string
-  file_size: number
+  mimeType: string
+  fileSize: number
 }
 
 // @ts-ignore
 export type Animation = {
-  file_id: string
-  file_unique_id: string
+  fileId: string
+  fileUniqueId: string
   width: Number
   height: number
   duration: number
   thumb:	PhotoSize
-  file_name: string
-  mime_type: string
-  file_size: number
+  fileName: string
+  mimeType: string
+  fileSize: number
 }
 
 export type Voice = {
-  file_id: string
-  file_unique_id: string
+  fileId: string
+  fileUniqueId: string
   duration: number
-  mime_type: string
-  file_size: number
+  mimeType: string
+  fileSize: number
 }
 
 export type VideoNote = {
-  file_id: string
-  file_unique_id: string
+  fileId: string
+  fileUniqueId: string
   length: number
   duration: number
   thumb:	PhotoSize
-  file_size: number
+  fileSize: number
 }
 
 export type Contact = {
-  phone_number: string
-  first_name: string
-  last_name: string
-  user_id: number
+  phoneNumber: string
+  firstName: string
+  lastName: string
+  userId: number
   vcard: string
 }
 
@@ -326,50 +360,50 @@ export type Venue = {
   location: Location
   title: string
   address: string
-  foursquare_id: string
-  foursquare_type: string
+  foursquareId: string
+  foursquareType: string
 }
 
 export type PollOption = {
   text: string
-  voter_count: number
+  voterCount: number
 }
 
 export type PollAnswer = {
-  poll_id: string
+  pollId: string
   user:	User
-  option_ids: Array<number>
+  optionIds: Array<number>
 }
 
 export type Poll = {
   id: string
   question: string
   options:	Array<PollOption>
-  total_voter_count: number
-  is_closed: boolean
-  is_anonymous: boolean
+  totalVoterCount: number
+  isClosed: boolean
+  isAnonymous: boolean
   type: string
-  allows_multiple_answers: boolean
-  correct_option_id: number
+  allowsMultipleAnswers: boolean
+  correctOptionId: number
 }
 
 export type UserProfilePhotos = {
-  total_count: number
+  totalCount: number
   photos: Array<Array<PhotoSize>>
 }
 
 // @ts-ignore
 export type File = {
-  file_id: string
-  file_unique_id: string
-  file_size: number
-  file_path: string
+  fileId: string
+  fileUniqueId: string
+  fileSize: number
+  filePath: string
 }
 
 export type KeyboardButton = {
   keyboard: Array<Array<KeyboardButton>>
-  resize_keyboard: boolean
-  one_time_keyboard: boolean
+  resizeKeyboard: boolean
+  oneTimeKeyboard: boolean
   selective: boolean
 }
 
@@ -378,29 +412,29 @@ export type KeyboardButtonPollType = {
 }
 
 export type ReplyKeyboardRemove = {
-  remove_keyboard: boolean
+  removeKeyboard: boolean
   selective: boolean
 }
 
 export type ReplyKeyboardMarkup = {
   keyboard: Array<Array<KeyboardButton>>;
-  resize_keyboard?: boolean;
-  one_time_keyboard?: boolean;
+  resizeKeyboard?: boolean;
+  oneTimeKeyboard?: boolean;
   selective?: boolean;
 }
 
 export type InlineKeyboardMarkup = {
-  inline_keyboard:	Array<Array<InlineKeyboardButton>>
+  inlineKeyboard:	Array<Array<InlineKeyboardButton>>
 }
 
 export type InlineKeyboardButton = {
   text: string
   url: string
-  login_url:	LoginUrl
-  callback_data: string
-  switch_inline_query: string
-  switch_inline_query_current_chat: string
-  callback_game:	CallbackGame
+  loginUrl:	LoginUrl
+  callbackData: string
+  switchInlineQuery: string
+  switchInlineQueryCurrentChat: string
+  callbackGame:	CallbackGame
   pay: boolean
 }
 
@@ -408,64 +442,64 @@ export type CallbackGame = {}
 
 export type LoginUrl = {
   url: string
-  forward_text: string
-  bot_username: string
-  request_write_access: boolean
+  forwardText: string
+  botUsername: string
+  requestWriteAccess: boolean
 }
 
 export type CallbackQuery = {
   id: string
   from: User
   message: Message
-  inline_message_id: string
-  chat_instance: string
+  inlineMessageId: string
+  chatInstance: string
   data: string
-  game_short_name: string
+  gameShortName: string
 }
 
 export type ForceReply = {
-  force_reply: boolean
+  forceReply: boolean
   selective: boolean
 }
 
 export type ChatPhoto = {
-  small_file_id: string
-  small_file_unique_id: string
-  big_file_id: string
-  big_file_unique_id: string
+  smallFileId: string
+  smallFileUniqueId: string
+  bigFileId: string
+  bigFileUniqueId: string
 }
 
 export type ChatMember = {
   user: User
   status: string
-  custom_title: string
-  until_date: number
-  can_be_edited: boolean
-  can_post_messages: boolean
-  can_edit_messages: boolean
-  can_delete_messages: boolean
-  can_restrict_members: boolean
-  can_promote_members: boolean
-  can_change_info: boolean
-  can_invite_users: boolean
-  can_pin_messages: boolean
-  is_member: boolean
-  can_send_messages: boolean
-  can_send_media_messages: boolean
-  can_send_polls: boolean
-  can_send_other_messages: boolean
-  can_add_web_page_previews: boolean
+  customTitle: string
+  untilDate: number
+  canBeEdited: boolean
+  canPostMessages: boolean
+  canEditMessages: boolean
+  canDeleteMessages: boolean
+  canRestrictMembers: boolean
+  canPromoteMembers: boolean
+  canChangeInfo: boolean
+  canInviteUsers: boolean
+  canPinMessages: boolean
+  isMember: boolean
+  canSendMessages: boolean
+  canSendMediaMessages: boolean
+  canSendPolls: boolean
+  canSendOtherMessages: boolean
+  canAddWebPagePreviews: boolean
 }
 
 export type ChatPermissions = {
-  can_send_messages: boolean
-  can_send_media_messages: boolean
-  can_send_polls: boolean
-  can_send_other_messages: boolean
-  can_add_web_page_previews: boolean
-  can_change_info: boolean
-  can_invite_users: boolean
-  can_pin_messages: boolean
+  canSendMessages: boolean
+  canSendMediaMessages: boolean
+  canSendPolls: boolean
+  canSendOtherMessages: boolean
+  canAddWebPagePreviews: boolean
+  canChangeInfo: boolean
+  canInviteUsers: boolean
+  canPinMessages: boolean
 }
 
 export type ResponseParameters = {
@@ -473,7 +507,7 @@ export type ResponseParameters = {
   retryAfter: number
 }
 
-export type InputFile = any;
+export type InputFile = Blob | File | ReadStream | Buffer | string;
 
 export type InputMedia = InputMediaPhoto | InputMediaVideo | InputMediaAnimation | InputMediaAudio | InputMediaDocument;
 
